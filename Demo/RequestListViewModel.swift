298ebe874b5bf283d7865c902a1f07edf7a8453d
y-hryk <dev.hy630823@gmail.com>
2016-11-27 20:30:53 +0900

    アカウントの数の変化を監視するように変更

---
+++ Demo/RequestListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RequestListViewModel.swift
+//  Demo
+//
+//  Created by yamaguchi on 2016/11/25.
+//  Copyright © 2016年 h.yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class RequestListViewModel: NSObject {
+
+}
